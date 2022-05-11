51e795cf85d5eb819eb801a02b4e58dc4c4d9c80
Jennifer Amis <jenamis@Jennifer-Amiss-MacBook-Pro.local>
2022-05-11 12:04:46 -0700

    Updated R Script and added suspension coil analysis screenshots

--- CarAnalysisScript.R
+++ CarAnalysisScript.R
@@ -10,3 +10,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = car_mpg))
+
+# Import and read Suspension_Coil CSV as table
+suspension_coil <- read.csv(file = "Resources/Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+head(suspension_coil)
+
+# Create total summary DF using summarize() function
+total_summary <- summarize(suspension_coil, Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# Create lot_summary DF using group_by() and summarize() functions
+lot_summary <- group_by(suspension_coil, Manufacturing_Lot) %>% 
+  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
