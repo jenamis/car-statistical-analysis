362a9a469a4fc82f021f4ade16df857bce77c68a
Jennifer Amis <jenamis@Jennifer-Amiss-MacBook-Pro.local>
2022-05-11 13:45:26 -0700

    Updated R Script and added t-test screenshots

--- CarAnalysisScript.R
+++ CarAnalysisScript.R
@@ -21,3 +21,18 @@ total_summary <- summarize(suspension_coil, Mean = mean(PSI), Median = median(PS
 # Create lot_summary DF using group_by() and summarize() functions
 lot_summary <- group_by(suspension_coil, Manufacturing_Lot) %>% 
   summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# Use t-test to compare mean PSI across all lots with population mean PSI of 1500
+t.test(suspension_coil$PSI, mu = 1500)
+
+# Use t-test to compare mean PSI for Lot 1 with population mean PSI of 1500
+lot1 = subset(suspension_coil, Manufacturing_Lot == "Lot1")
+t.test(lot1$PSI, mu = 1500)
+
+# Use t-test to compare mean PSI for Lot 2 with population mean PSI of 1500
+lot2 = subset(suspension_coil, Manufacturing_Lot == "Lot2")
+t.test(lot2$PSI, mu = 1500)
+
+# Use t-test to compare mean PSI for Lot 3 with population mean PSI of 1500
+lot3 = subset(suspension_coil, Manufacturing_Lot == "Lot3")
+t.test(lot3$PSI, mu = 1500)
